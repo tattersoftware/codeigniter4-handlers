6603252e5da7426734fe7fbed6c92099d4469d0d
MGatner <mgatner@icloud.com>
2021-09-13 23:13:39 +0000

    Add coverage

--- tests/unit/BaseHandlerTest.php
+++ tests/unit/BaseHandlerTest.php
@@ -18,4 +18,16 @@ final class BaseHandlerTest extends HandlerTestCase
 
 		$this->assertSame('bar', $handler->foo);
 	}
+
+	public function testIsset()
+	{
+		$handler = new class () extends BaseHandler {
+			protected $attributes = [
+				'fruit' => 'banana',
+			];
+		};
+
+		$this->assertTrue(isset($handler->fruit));
+		$this->assertFalse(isset($handler->meat));
+	}
 }
